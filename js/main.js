42f947930f5ddb115184fe8e670df6ef45c797a4
carlOzil <seifer01es@msn.com>
2023-06-28 23:40:39 +0200

    second chance

--- js/main.js
+++ js/main.js
@@ -1,26 +1,94 @@
 
+// VARIABLES --->>>
 
-const contenedorViajes = document.querySelector('#contenedor-viajes')
+const contenedorViajes = document.querySelector('#contenedor-viajes');
 
-console.log(contenedorViajes)
+const contenedorBienvenidos = document.querySelector('#contenedor-welcome');
 
-const arrayViajes = [1, 2, 3]
+const contenedorDestinos = document.querySelector('#select-destinos');
 
 const fragment = document.createDocumentFragment();
 
-console.log(fragment)
+
+// ARRAYS --->>>
+
+const arrayBienvenidos = [{
+    url: './img/3.jpg',
+    alt: 'cielo al anochecer'
+},
+{
+    url: './img/5.jpg',
+    alt: 'luna entre nubes'
+},
+{
+    url: './img/7.jpg',
+    alt: 'bandada de pajaros al atardecer'
+},
+{
+    url: './img/8.jpg',
+    alt: 'rama de castañas en blanco y negro'
+}];
+
+
+const arrayViajes = [{
+    url: './img/viajes-1.jpg',
+    alt: 'mujer en hamaca',
+    msg: 'Playas paradisiacas'
+},
+{
+    url: './img/viajes-2.jpg',
+    alt: 'pasarela hacia a cabañas sobre el agua',
+    msg: 'Las maldivas'
+},
+{
+    url: './img/viajes-3.jpg',
+    alt: 'paseo de acantilado con paso a nivel',
+    msg: 'Pasos a nivel naturales'
+}];
+
+
+const arrayDestinos = [{
+    value: 'op1',
+    text: 'Granada'
+},
+{
+    value: 'op2',
+    text: 'Bogotá'
+},
+{
+    value: 'op3',
+    text: 'Amsterdam'
+},
+{
+    value: 'op4',
+    text: 'opcion 3'
+
+
+}];
+
+
+// FUNCIONES --->>>
+
+const pintarPostales=()=>{
 
 arrayViajes.forEach((item) => {
-    const cajaFoto = document.createElement('FIGURE');
-    cajaFoto.classList.add('borde');
+    const cajon = document.createElement('FIGURE');
+    cajon.classList.add('postal');
+    const caja = document.createElement('DIV');
+    const imagen = document.createElement('IMG');
 
+    imagen.src = `img/${item.url}`
 
-    
-    fragment.append(cajaFoto)
+    caja.append(imagen);
+    cajon.append(caja)
+    fragment.append(cajon)
+
 })
 
-console.log(fragment)
-
 contenedorViajes.append(fragment)
+}
 
-console.log(fragment)
+
+// INVOCACIONES --->>>
+
+pintarPostales()
